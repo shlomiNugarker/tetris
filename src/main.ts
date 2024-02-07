129ba01595cba4ce9679b545ce25c43b5628115f
Shlomi Nugarker <shlomin1231@gmail.com>
2024-02-07 03:48:59 +0200

    animate canvas

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,23 @@
 import './style.css'
 import './tetris/index'
+import { Game } from './tetris/index'
+
+const canvas = <HTMLCanvasElement>document.getElementById('canvas')
+const ctx = canvas.getContext('2d')!
+canvas.width = 500
+canvas.height = 500
+
+const game = new Game()
+
+let lastTime = 0
+
+function animate(timeStamp: number) {
+  const deltaTime = timeStamp - lastTime
+  lastTime = timeStamp
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  game.update(deltaTime)
+  game.draw(ctx)
+  if (!game.isGameOver) requestAnimationFrame(animate)
+}
+
+animate(0)
