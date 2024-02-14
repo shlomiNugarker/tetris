cdd5306b7b0249917b17211a4adc5d000eae4e8a
Shlomi Nugarker <shlomin1231@gmail.com>
2024-02-14 19:35:43 +0200

    startNewGame()

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,28 @@ import './style.css'
 import './tetris/index'
 import { Game } from './tetris/index'
 
-const canvas = <HTMLCanvasElement>document.getElementById('canvas')
+function startNewGame() {
+  const canvas = <HTMLCanvasElement>document.getElementById('canvas')
+  let game = new Game(canvas)
 
-const game = new Game(canvas)
+  let lastTime = 0
 
-let lastTime = 0
+  function gameLoop(timeStamp: number) {
+    const deltaTime = timeStamp - lastTime
+    lastTime = timeStamp
+    game.update(deltaTime, timeStamp)
+    game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height)
+    game.draw()
+    if (!game.isGameOver) {
+      requestAnimationFrame(gameLoop)
+    } else {
+      game = new Game(canvas)
+      lastTime = 0
+      requestAnimationFrame(gameLoop)
+    }
+  }
 
-// Game loop function
-function gameLoop(timeStamp: number) {
-  const deltaTime = timeStamp - lastTime
-  lastTime = timeStamp
-  game.update(deltaTime, timeStamp)
-  game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height)
-  game.draw()
-  if (!game.isGameOver) requestAnimationFrame(gameLoop)
+  gameLoop(0)
 }
 
-gameLoop(0)
+startNewGame()
